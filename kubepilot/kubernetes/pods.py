9fdae94a2ce10dea6809f3e9f69189ea698318b2
Rakshith S <113280385+rakshithcodes@users.noreply.github.com>
2026-08-15 23:41:19 +0530

    add describe pod

--- kubepilot/kubernetes/pods.py
+++ kubepilot/kubernetes/pods.py
@@ -17,3 +17,22 @@ def list_pods(
         pods = v1.list_namespaced_pod(namespace=current_namespace)
     for pod in pods.items:
         print(f"{pod.metadata.namespace}\t{pod.metadata.name}")
+
+ def describe_pod(
+    name: str,
+    ns: str = None,
+    ):
+    v1 = get_core_v1_api()
+    if ns:
+        pod = v1.read_namespaced_pod(name=name, namespace=ns)
+    else:
+        current_namespace = get_current_namespace()
+        pod = v1.read_namespaced_pod(name=name, namespace=current_namespace)
+    print(f"Name: {pod.metadata.name}")
+    print(f"Namespace: {pod.metadata.namespace}")
+    print(f"Status: {pod.status.phase}")
+    print(f"Node: {pod.spec.node_name}")
+    print(f"Containers:")
+    for container in pod.spec.containers:
+        print(f"  - Name: {container.name}")
+        print(f"    Image: {container.image}")       
